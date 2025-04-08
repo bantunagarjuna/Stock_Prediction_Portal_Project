021886558ee9f2f9a064dc3d861b55dc7aa096f5
bantunagarjuna <bantunagarjuna2@gmail.com>
2025-04-08 17:55:12 +0530

    home page react

---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <footer className='footer my-1'>
+        <hr className='border-bottom'/>
+        <p className='text-light text-center'>&copy; 2025 - Built with ❤️ by Bantu Nagarjuna</p>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from './Button'
+
+const main = () => {
+  return (
+    <>
+      <div className='container'>
+        <div className='p-5 text-center bg-light-dark rounded-3'>
+            <h1 className='text-light'>Stock Prediction Portal</h1>
+            <p className='text-light lead'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto perferendis, alias nam, adipisci unde minima architecto ut asperiores optio est consequatur cupiditate distinctio eligendi dolore eum corrupti consectetur tempore totam!</p>
+            < Button text="Login" class='btn-outline-light' />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default main
